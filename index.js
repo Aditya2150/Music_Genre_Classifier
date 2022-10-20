b389335081dfb0d3cb04c3e93c5bb45d30bb0eb9
Rahul Raj Tripathi <31700666+rahulwittycoder@users.noreply.github.com>
2022-10-20 13:27:39 +0530

    removed comments

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-// const http = require('http');
-// const express = require('express');
-// const app = express();
-// const server = http.createServer(app);
-// server.listen(3000);
+
 const fs = require('fs');
 let fileWrite = () => {
     let str = "warAxe";
